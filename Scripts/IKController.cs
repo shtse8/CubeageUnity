1d2b00927083a6f44da4a942758a7d6e39a0139a
shtse8 <shtse8@outlook.com>
2021-01-08 02:09:30 +0800

    Added IKController.cs and IKControllerHost.cs

---
+++ Scripts/IKController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cubeage
+{
+    [ExecuteInEditMode]
+    public class IKController : MonoBehaviour
+    {
+        public bool enabledIK = true;
+
+        public Transform target;
+
+        [Range(0.0f, 1.0f)]
+        public float weight = 1;
+
+        public bool leftHand;
+        public bool rightHand;
+        public bool leftFoot;
+        public bool rightFoot;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        public bool IsEnable(AvatarIKGoal goal)
+        {
+            if (!enabledIK && target != null)
+                return false;
+            switch (goal)
+            {
+                case AvatarIKGoal.LeftFoot:
+                    return leftFoot;
+                case AvatarIKGoal.RightFoot:
+                    return rightFoot;
+                case AvatarIKGoal.LeftHand:
+                    return leftHand;
+                case AvatarIKGoal.RightHand:
+                    return rightHand;
+                default:
+                    throw new System.Exception("unknown goal");
+            }
+        }
+    }
+}
---
+++ Scripts/IKControllerHost.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Cubeage
+{
+    [ExecuteInEditMode]
+    public class IKControllerHost : MonoBehaviour
+    {
+        public Animator animator;
+        public IKController[] controllers;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = gameObject.GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            animator.Update(0);
+        }
+
+        private void OnAnimatorIK(int layerIndex)
+        {
+            var dict = EnumHelper.GetValues<AvatarIKGoal>().ToDictionary(x => x, x => false);
+            if (animator)
+            {
+                foreach (var controller in controllers) 
+                {
+                    foreach (var goal in EnumHelper.GetValues<AvatarIKGoal>())
+                    {
+                        if (controller.IsEnable(goal))
+                        {
+                            dict[goal] = true;
+                            animator.SetIKPositionWeight(goal, controller.weight);
+                            animator.SetIKRotationWeight(goal, controller.weight);
+                            if (controller.target != null)
+                            {
+                                animator.SetIKPosition(goal, controller.target.position);
+                                animator.SetIKRotation(goal, controller.target.rotation);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach(var goal in dict.Where(x => !x.Value).Select(x => x.Key))
+            {
+                animator.SetIKPositionWeight(goal, 0);
+                animator.SetIKRotationWeight(goal, 0);
+            }
+        }
+
+
+    }
+}
